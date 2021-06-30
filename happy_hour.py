0bd784f6a9945c48f49e68d0b7e4af2bb7182fae
GroosL <leonardog_rs@yahoo.com.br>
2021-06-30 15:37:53 -0300

    no more duplicates

--- happy_hour.py
+++ happy_hour.py
@@ -17,4 +17,7 @@ random_bar = random.choice(bars)
 random_person = random.choice(people)
 random_person2 = random.choice(people)
 
+while random_person == random_person2:
+    random_person2 = random.choice(people)
+
 print(f"How about you go to {random_bar} with {random_person} and {random_person2}")
