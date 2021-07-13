63d22ddc236a14608dd83e1243f9267a811d9195
GroosL <leonardog_rs@yahoo.com.br>
2021-07-13 19:29:30 -0300

    Week 2 begins

---
+++ week2/if.py
@@ -0,0 +1,12 @@
+answer = input("Do you want to hear a joke? ")
+
+t=answer.upper()
+
+if t == "YES":
+    print("I'm against picketing, but I don't know how to show it.")
+
+elif t == "NO":
+    print("Fine.")
+
+else:
+    print("Not a proper answer")
